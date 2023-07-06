34be752deb511c36785ce16f90f615cb9ad13bac
les <130134817+leslee-glints@users.noreply.github.com>
2023-07-07 03:14:24 +0800

    fix: Error: Commented-out code is forbidden

--- src/components/templates/transactions/Transactions.tsx
+++ src/components/templates/transactions/Transactions.tsx
@@ -70,20 +70,7 @@ const Transactions = () => {
                     label: '# of Votes',
                     data: [12, 19, 3, 5, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     borderWidth: 1,
-                    backgroundColor: [
-                      // 'rgba(255, 99, 132, 0.2)',
-                      'rgba(255, 159, 64, 0.2)',
-                      // 'rgba(255, 205, 86, 0.2)',
-                      // 'rgba(75, 192, 192, 0.2)',
-                      // 'rgba(54, 162, 235, 0.2)',
-                      // 'rgba(153, 102, 255, 0.2)',
-                      // 'rgba(201, 203, 207, 0.2)',
-                      // 'rgba(255, 99, 132, 0.2)',
-                      // 'rgba(255, 159, 64, 0.2)',
-                      // 'rgba(255, 205, 86, 0.2)',
-                      // 'rgba(75, 192, 192, 0.2)',
-                      // 'rgba(54, 162, 235, 0.2)'
-                    ],
+                    backgroundColor: ['rgba(255, 159, 64, 0.2)'],
                   },
                   {
                     label: '# of Votes',
@@ -147,24 +134,7 @@ const Transactions = () => {
                       'rgba(255, 205, 86, 0.2)',
                       'rgba(75, 192, 192, 0.2)',
                     ],
-                    // backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    // borderColor: 'rgb(255, 99, 132)',
-                    // pointBackgroundColor: 'rgb(255, 99, 132)',
-                    // pointBorderColor: '#fff',
-                    // pointHoverBackgroundColor: '#fff',
-                    // pointHoverBorderColor: 'rgb(255, 99, 132)'
                   },
-                  // {
-                  //   label: 'Gas Used',
-                  //   data: [28, 48, 40, 19, 96, 27, 100, 50, 10],
-                  //   fill: true,
-                  //   // backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                  //   // borderColor: 'rgb(54, 162, 235)',
-                  //   // pointBackgroundColor: 'rgb(54, 162, 235)',
-                  //   // pointBorderColor: '#fff',
-                  //   // pointHoverBackgroundColor: '#fff',
-                  //   // pointHoverBorderColor: 'rgb(54, 162, 235)'
-                  // }
                 ],
               }}
             />
